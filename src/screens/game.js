1cf73ab57b317c1dd199a031cfd0f2ac2acd3129
Dominic Charley-Roy <dominiccharleyroy@gmail.com>
2014-04-25 12:45:19 -0400

    Pressing ? in game screen now goes to help screen.

--- src/screens/game.js
+++ src/screens/game.js
@@ -5,35 +5,89 @@ function GameScreen() {
 };
 GameScreen.extend(Screen);
 
+GameScreen.prototype._enter = function() {
+  // If we have a level, start animating
+  if (Game.currentLevel) {
+    this._startAnimationTimer(Game.currentLevel);
+  }
+};
+
+GameScreen.prototype._exit = function(first_argument) {
+  // Stop any animation timer
+  this._stopAnimationTimer();
+};
+
 /**
- * This gets called when the game changes level.
+ * Stops any ongoing animation timers.
  */
-GameScreen.prototype.changeLevel = function() {
+GameScreen.prototype._stopAnimationTimer = function() {
   if (this._animationTimer) {
     clearInterval(this._animationTimer);
     this._animationTimer = null;
   }
-  // Test if we need to animate again
-  if (Game.currentLevel.isAnimated()) {
-    var totalFrames = Game.currentLevel.getAnimationFrames();
+};
 
-    this._animationFrame = 0;    
+/**
+ * Starts the animation timer for a given level
+ * @param  {Level} level
+ */
+GameScreen.prototype._startAnimationTimer = function(level) {
+  // Test if we need to animate again
+  this._animationFrame = 0;    
+  if (level.isAnimated()) {
+    var totalFrames = level.getAnimationFrames();
 
     this._animationTimer = setInterval(function() {
       this._animationFrame = (this._animationFrame + 1) % totalFrames;
       Game.display.clear();
       this.render();
-    }.bind(this), Math.round(1000 / Game.currentLevel.getAnimationFrames()));
+    }.bind(this), Math.round(1000 / level.getAnimationFrames()));
   }
 };
 
+/**
+ * This gets called when the game changes level.
+ */
+GameScreen.prototype.changeLevel = function() {
+  this._stopAnimationTimer();
+  this._startAnimationTimer(Game.currentLevel);
+};
+
+/**
+ * @return {int} The current animation frame.
+ */
 GameScreen.prototype.getAnimationFrame = function() {
   return this._animationFrame;
 };
 
+/**
+ * @override
+ */
 GameScreen.prototype.render = function() {
   // If no current level, don't render
   if (!Game.currentLevel) return;
   // Render the level
   Game.currentLevel.draw();
+};
+
+/**
+ * Helper function for switching back to the game screen.
+ */
+GameScreen.prototype._enterAndRender = function() {
+  this.enter();
+  this.render();
+};
+
+/**
+ * @override
+ */
+GameScreen.prototype.handleEvent = function(e) {
+  switch (e.keyCode) {
+    case ROT.VK_QUESTION_MARK: 
+    case ROT.VK_SLASH: 
+      e.preventDefault();
+      this.exit();
+      new HelpScreen(false).enter().then(this._enterAndRender.bind(this));
+      break;
+  }
 };
